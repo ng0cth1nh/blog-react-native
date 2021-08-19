58a52fab2650c47ad32c2146eae3dae62bf473da
Vu Ngoc Thinh <vnt462@gmail.com>
2021-08-19 11:03:55 +0700

    add index screen

---
+++ src/context/BlogContext.js
@@ -0,0 +1,26 @@
+import React, { useReducer } from 'react';
+
+const BlogContext = React.createContext();
+
+const blogReducer = (state, action) => {
+    switch(action.type){
+        case 'add_blog_post':
+            return [...state,{title: `blog post #${state.length + 1}`}];
+        default:
+            return state;    
+    }
+
+}
+
+export const BlogProvider = ({ children }) => {
+
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
+    const addBlogPost = () => {
+        dispatch({type: 'add_blog_post'});
+    }
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+        {children}
+    </BlogContext.Provider>;
+}
+
+export default BlogContext;
